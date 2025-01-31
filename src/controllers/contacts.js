26a756b3048d4186d910860e772f21610da29195
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-01-31 16:12:26 +0200

    add error handling

---
+++ src/controllers/contacts.js
@@ -0,0 +1,27 @@
+import { getContactById, getContacts } from '../db/services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getContacts();
+
+  return res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
